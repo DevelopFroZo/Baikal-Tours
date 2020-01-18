b0bc4ae87961aef8d3a5d8ec5c0638ecfa3df049
Max Zhukov <promakc31@gmail.com>
2020-01-18 13:04:08 +0800

    create filter at frontend side

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,12 @@
   import Footer from "../components/footer.svelte";
   import Card from "../components/card_of_event.svelte";
 
+  let date = "",
+    price = "",
+	showFilter = false,
+	priceStart = "",
+	priceEnd = "";
+
   let cards = [
     {
       id: 1,
@@ -41,6 +47,162 @@
       place: "Иркутск, о. Конный"
     }
   ];
+
+  let options = [
+    {
+      isVisible: false,
+      option: null,
+      btn: null
+    },
+    {
+      isVisible: false,
+      option: null,
+      btn: null
+    },
+    {
+      isVisible: false,
+      option: null,
+      btn: null
+    }
+  ];
+
+  let filter = [
+    [
+      {
+        value: "",
+        active: false
+      },
+      {
+        value: "",
+        active: false
+      }
+    ],
+    [
+      {
+        value: "Иркутск",
+        active: false
+      },
+      {
+        value: "Ангарск",
+        active: false
+      },
+      {
+        value: "Шелехов",
+        active: false
+      }
+    ],
+    [
+      {
+        value: "С семьей",
+        active: false
+      },
+      {
+        value: "Одному",
+        active: false
+      },
+      {
+        value: "С друзьями",
+        active: false
+      }
+    ],
+    [
+      {
+        value: "фестивали и праздники",
+        active: false
+      },
+      {
+        value: "отдых",
+        active: false
+      },
+      {
+        value: "форум",
+        active: false
+      }
+    ],
+    [
+      {
+        value: "",
+        active: false
+      },
+      {
+        value: "",
+        active: false
+      }
+    ]
+  ];
+
+  function hideAll(e) {
+    for (let i = 0; i < options.length; i++) {
+      e = e || event;
+      let target = e.target || e.srcElement;
+      const its_menu =
+        target == options[i].option || options[i].option.contains(target);
+      const its_btnMenu = target == options[i].btn;
+      if (!its_menu && !its_btnMenu) options[i].isVisible = false;
+    }
+  }
+
+  $: {
+    //change date status and her correct view
+    if (
+      new Date(filter[0][0].value) > new Date(filter[0][1].value) &&
+      filter[0][0].value !== "" &&
+      filter[0][1].value !== ""
+    )
+      filter[0][0].value = filter[0][1].value;
+
+    filter[0][0].active = filter[0][0].value === "" ? false : true;
+    filter[0][1].active = filter[0][1].value === "" ? false : true;
+
+    if (filter[0][0].active && filter[0][1].active)
+      date = filter[0][0].value + " - " + filter[0][1].value;
+    else if (filter[0][0].active) date = filter[0][0].value;
+    else if (filter[0][1].active) date = filter[0][1].value;
+    else date = "";
+
+    //change price status and her correct
+
+    if (
+      filter[4][0].value > filter[4][1].value &&
+      filter[4][0].value !== "" &&
+      filter[4][1].value !== ""
+    ){
+		priceStart = filter[4][1].value;
+		filter[4][0].value = filter[4][1].value;
+	}
+
+	if(filter[4][0].value === "" || filter[4][0].value === undefined)
+		filter[4][0].active = false;
+	else
+		filter[4][0].active = true;
+
+	if(filter[4][1].value === "" || filter[4][1].value === undefined)
+		filter[4][1].active = false;
+	else
+		filter[4][1].active = true;
+
+    if (filter[4][0].active && filter[4][1].active)
+      price = "от " + filter[4][0].value + "р до " + filter[4][1].value + "р";
+    else if (filter[4][0].active) price = "от " + filter[4][0].value + "р";
+    else if (filter[4][1].active) price = "до" + filter[4][1].value + "р";
+    else price = "";
+
+	//show active filters
+    showFilter = false;
+    for (let i = 0; i < filter.length; i++) {
+      for (let j = 0; j < filter[i].length; j++) {
+        if (filter[i][j].active) {
+          showFilter = true;
+          break;
+        }
+      }
+    }
+  }
+
+  function setPrice(){
+	  filter[4][0].value = priceStart;
+	  filter[4][1].value = priceEnd;
+  }
 </script>
 
 <style lang="scss">
@@ -58,8 +220,9 @@
     grid-row-gap: 41px;
   }
 
-  input {
-    border: 1px solid #7b7b7b;
+  input,
+  .select {
+    border: 1px solid #7b7b7b66;
     background: white;
     font-style: italic;
     padding: 0 3px;
@@ -73,11 +236,11 @@
     &.date {
       width: 70px;
       position: relative;
-	}
-	
-	&[type="number"]{
-		width: 70px;
-	}
+    }
+
+    &[type="number"] {
+      width: 70px;
+    }
   }
 
   .date::-webkit-inner-spin-button,
@@ -94,7 +257,8 @@
     transform: translateY(-50%);
   }
 
-  #date-end, #price-end {
+  #date-end,
+  #price-end {
     margin-left: -5px;
   }
 
@@ -106,21 +270,91 @@
   .filters {
     background: $Light_Gray;
     padding: 18px 20px;
-	margin: 20px 0;
-	display: flex;
-	justify-content: space-between;
+    margin: 20px 0 15px;
+    display: flex;
+    justify-content: space-between;
   }
 
-  select{
-	  background: white;
-	  width: 140px;
-	  font-style: italic;
+  .select {
+    width: 140px;
+    height: 25px;
+    text-align: left;
+	position: relative;
+	color: #00000099;
+
+    &::before {
+      position: absolute;
+      content: " ";
+      width: 10px;
+      height: 4px;
+      top: 50%;
+      right: 5px;
+      transform: translateY(-50%);
+      background-image: url(../img/change.png);
+      background-size: 100% 100%;
+    }
+  }
+
+  .select-block {
+    position: relative;
+  }
+
+  .option {
+    position: absolute;
+    top: 27px;
+    left: 0;
+    background: white;
+    border: 1px solid #7b7b7b;
+    width: 100%;
+    box-sizing: border-box;
+    visibility: hidden;
+
+    & > div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 13px;
+      padding: 0 5px;
+    }
+  }
+
+  .option-visible {
+    visibility: visible;
+  }
+
+  .active-filter-block {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    font-style: italic;
+    font-size: $Medium_Font_Size;
+    margin-bottom: 28px;
+
+    & > .filter-head {
+      margin-top: 5px;
+    }
+  }
+
+  .active-filter {
+    padding: 3px 7px;
+    background: $Medium_Gray;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    margin-left: 8px;
+    margin-top: 5px;
+
+    & > button {
+      margin-left: 7px;
+    }
   }
 </style>
 
 <svelte:head>
   <title>Каталог событий</title>
 </svelte:head>
+
+<svelte:window on:click={hideAll} />
 
 <Header />
 <div class="form-width">
@@ -131,17 +365,18 @@
         placeholder="Дата с"
         class="date"
         type="text"
+        bind:value={filter[0][0].value}
         on:focus={function(e) {
           e.target.type = 'date';
         }}
         on:blur={function(e) {
           e.target.type = 'text';
-        }}
-        id="date-start" />
+        }} />
       <input
         placeholder="Дата по"
         class="date"
         type="text"
+        bind:value={filter[0][1].value}
         on:focus={function(e) {
           e.target.type = 'date';
         }}
@@ -150,29 +385,135 @@
         }}
         id="date-end" />
     </div>
-	<div>
-		<button class = "select">где</button>
-		<div class = "option">
-			
-		</div>
-	</div>
-	<select id = "kompany">
-		<option disabled selected>с кем</option>
-		<option>Один</option>
-		<option>С семьей</option>
-		<option>С друзьями</option>
-	</select>
-	<select id = "theme">
-		<option disabled selected>тематика</option>
-		<option>Фестивали и праздники</option>
-		<option>Форумы</option>
-		<option>Путешествия</option>
-	</select>
-	<div>
-		<input type="number" placeholder="цена от" id="price-start"/>
-		<input type="number" placeholder="до" id = "price-end"/>
-	</div>
+    <div class="select-block">
+      <button
+        class="select"
+        bind:this={options[0].btn}
+        on:click={() => {
+          options[0].isVisible = true;
+        }}>
+        где
+      </button>
+      <div
+        class="option"
+        class:option-visible={options[0].isVisible}
+        bind:this={options[0].option}>
+        {#each filter[1] as city}
+          <div>
+            <label>{city.value}</label>
+            <input type="checkbox" bind:checked={city.active} />
+          </div>
+        {/each}
+      </div>
+    </div>
+    <div class="select-block">
+      <button
+        class="select"
+        bind:this={options[1].btn}
+        on:click={() => {
+          options[1].isVisible = true;
+        }}>
+        с кем
+      </button>
+      <div
+        class="option"
+        class:option-visible={options[1].isVisible}
+        bind:this={options[1].option}>
+        {#each filter[2] as companios}
+          <div>
+            <label>{companios.value}</label>
+            <input type="checkbox" bind:checked={companios.active} />
+          </div>
+        {/each}
+      </div>
+    </div>
+    <div class="select-block">
+      <button
+        class="select"
+        bind:this={options[2].btn}
+        on:click={() => {
+          options[2].isVisible = true;
+        }}>
+        тематика
+      </button>
+      <div
+        class="option"
+        class:option-visible={options[2].isVisible}
+        bind:this={options[2].option}>
+        {#each filter[3] as subjects}
+          <div>
+            <label>{subjects.value}</label>
+            <input type="checkbox" bind:checked={subjects.active} />
+          </div>
+        {/each}
+      </div>
+    </div>
+    <div>
+      <input
+        type="number"
+        placeholder="цена от"
+        id="price-start"
+        bind:value={priceStart}
+		on:blur={setPrice} />
+      <input
+        type="number"
+        placeholder="до"
+        id="price-end"
+        bind:value={priceEnd}
+		on:blur={setPrice} />
+    </div>
   </div>
+  {#if showFilter}
+    <div class="active-filter-block">
+      <div class="filter-head">Вы выбрали:</div>
+      {#if date !== ''}
+        <div class="active-filter">
+          {date}
+          <button
+            class="delete-filter"
+            on:click={() => {
+              filter[0][0].value = '';
+              filter[0][1].value = '';
+            }}>
+            <img src="img/clear.png" />
+          </button>
+        </div>
+      {/if}
+      {#each filter as filt, i}
+        {#if i > 0 && i < 4}
+          {#each filt as fl, j}
+            {#if fl.active}
+              <div class="active-filter">
+                {fl.value}
+                <button
+                  class="delete-filter"
+                  on:click={() => {
+                    fl.active = false;
+                  }}>
+                  <img src="img/clear.png" />
+                </button>
+              </div>
+            {/if}
+          {/each}
+        {/if}
+      {/each}
+      {#if price !== ''}
+        <div class="active-filter">
+          {price}
+          <button
+            class="delete-filter"
+            on:click={() => {
+              priceStart = "";
+			  priceEnd = "";
+			  setPrice();
+            }}>
+            <img src="img/clear.png" />
+          </button>
+        </div>
+      {/if}
+    </div>
+  {/if}
+
   <div class="cards-block">
     {#each cards as cardInfo (cardInfo.id)}
       <Card {...cardInfo} />
