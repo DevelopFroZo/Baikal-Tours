91f71d64ba6c0d7dd7680db7c071fa721aea476c
Max Zhukov <promakc31@gmail.com>
2020-01-26 16:10:30 +0800

    now action page has data from database and add gallery in him. Add range of price on filter

--- src/helpers/parsers.js
+++ src/helpers/parsers.js
@@ -1,11 +1,32 @@
 export {
-    parseDate
+    parseDate,
+    dateOptions,
+    parseDateToDateAndDay
 }
 
-function parseDate(date){
+var dateOptions = {
+    era: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+};
+
+function parseDate(date) {
     return date.getFullYear() + "-" + dateFormat(parseInt(date.getMonth() + 1) + "") + "-" + dateFormat(date.getDate());
 }
 
-function dateFormat(date){
+function dateFormat(date) {
     return date.length === 1 ? "0" + date : date;
+}
+
+function parseDateToDateAndDay(date) {
+    date = new Date(date);
+    date = date.toLocaleString("ru", dateOptions).split(", ")[1].split(" ");
+    date = date[0] + " " + date[1];
+    return date;
 }
