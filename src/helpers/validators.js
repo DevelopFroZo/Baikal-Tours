ffffee816702106db41dea8b5dc8d72a24931dee
Max Zhukov <promakc31@gmail.com>
2020-01-30 11:57:25 +0800

    fix mail falidation

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -21,6 +21,7 @@ function validatePhone(e) {
 }
 
 function validateMail(mail){
+    mail = mail.trim();
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(mail).toLowerCase());
 }
