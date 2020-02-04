88e84408f3d2bdc547300303bdc4e903bd32058d
Max Zhukov <promakc31@gmail.com>
2020-02-04 09:11:47 +0800

    fixed critical bag with registration

--- src/routes/register/index.svelte
+++ src/routes/register/index.svelte
@@ -4,8 +4,6 @@
 
   const fetcher = new Fetcher();
 
-  export let locale;
-
   async function register(e) {
     let data = e.detail;
 
@@ -27,7 +25,7 @@
 </style>
 
 <svelte:head>
-  <title>{Регистрация}</title>
+  <title>Регистрация</title>
 </svelte:head>
 
 <div class="body">
