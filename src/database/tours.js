a8b6f0dd62bbc472bec4755d85dd1deb2fca973c
FroZo <frozo02@yandex.ru>
2020-04-09 00:56:43 +0800

    [B] Fix bug with tours

--- src/database/tours.js
+++ src/database/tours.js
@@ -24,11 +24,11 @@ export default class extends Foundation{
     const rows = ( await super.query(
       `select t.id, t.site, t.image_url, t.price, tt.name
       from
-        excursions as t,
-        excursions_translates as tt
+        tours as t,
+        tours_translates as tt
       where
         tt.locale = $1 and
-        t.id = tt.excursion_id
+        t.id = tt.tour_id
       order by date_start`,
       [ locale ]
     ) ).rows;
@@ -64,12 +64,12 @@ export default class extends Foundation{
     const rows = ( await super.query(
       `select t.id, t.site, t.image_url, t.price, tt.name
       from
-        excursions as t,
-        excursions_translates as tt
+        tours as t,
+        tours_translates as tt
       where
         tt.locale = $1 and
         ${filters}
-        t.id = tt.excursion_id
+        t.id = tt.tour_id
       order by date_start`,
       params
     ) ).rows;
