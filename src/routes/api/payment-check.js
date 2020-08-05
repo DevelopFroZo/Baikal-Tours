fabec4437cb6520f8279f39e2487dbf876713238
FroZo <frozo02@yandex.ru>
2020-08-06 01:48:08 +0800

    [B] Add mail when payment is success

--- src/routes/api/payment-check.js
+++ src/routes/api/payment-check.js
@@ -2,15 +2,19 @@
 
 import fetch from "node-fetch";
 import { getOrderStatus } from "/helpers/sber/acquiring/index";
+import fillers from "/mail_service/fillers/index";
+import { getTemplate, getTemplateTexts } from "/mail_service/index";
 
 export {
   get
 };
 
 async function get( {
-  session: { isLogged },
+  session: { locale, isLogged },
   query: { orderId },
-  database: { pool }
+  database: { pool },
+  mail,
+  _
 }, res ){
   if( !isLogged ) return res.json( {
     ok: false,
@@ -56,22 +60,64 @@ async function get( {
       paid,
       amount
     } ] } = await transaction.query(
-      `select
-      	ar.action_id, ar.name, ar.surname, ar.form_url, ar.paid,
-        sum( ab.price * arb.count )::int as amount
-      from
-      	action_reservations as ar,
-        action_reservations_buyable as arb,
-        action_buyable as ab
-      where
-      	order_id = $1 and
-        ar.id = arb.action_reservation_id and
-        arb.action_buyable_id = ab.id
-      group by ar.action_id, ar.name, ar.surname, ar.form_url, ar.paid`,
+      `select action_id, name, surname, form_url, paid
+      from action_reservations
+      where order_id = $1`,
       [ orderId ]
     );
 
     if( OrderStatus === 2 && !paid ){
+      const { rows: [ { date, phone, email } ] } = await transaction.query(
+        `select date, phone, email
+        from action_reservations
+        where action_id = $1`,
+        [ actionId ]
+      );
+
+      const { rows: buyable } = await transaction.query(
+        `select arb.count, ab.price, ab.type, abt.name
+        from
+          action_reservations as ar,
+          action_reservations_buyable as arb,
+          action_buyable as ab,
+          action_buyable_translates as abt
+        where
+          abt.locale = $1 and
+          ar.action_id = $2 and
+          ar.id = arb.action_reservation_id and
+          arb.action_buyable_id = ab.id and
+          ab.id = abt.action_buyable_id`,
+        [ locale, actionId ]
+      );
+
+      const { rows: [ { action_name } ] } = await transaction.query(
+        `select name as action_name
+        from actions_translates
+        where
+          locale = $1 and
+          action_id = $2`,
+        [ locale, actionId ]
+      );
+
+      const locations = ( await transaction.query(
+        `select
+        	l2.name ||
+        	case
+      			when al2.address is null then ''
+      			else ', ' || al2.address
+      		end as location
+        from
+        	actions_locations2 as al2,
+        	locations2 as l2
+        where
+        	l2.locale = $1 and
+        	al2.action_id = $2 and
+        	al2.location2_id = l2.id`,
+        [ locale, actionId ]
+      ) ).rows.map( ( { location } ) => location );
+
+      const amount = buyable.reduce( ( res, { count, price } ) => res + count * price, 0 );
+
       await transaction.query(
         `update action_reservations
         set
@@ -86,6 +132,34 @@ async function get( {
         set balance = balance + $1
         where id = $2`,
         [ amount, actionId ]
+      );
+
+      const templateName = "payment";
+      // #fix проверка
+      const filler = fillers[ templateName ];
+      // #fix проверка
+      const template = await getTemplate( templateName );
+      // #fix проверка
+      const { [ locale ]: texts } = await getTemplateTexts( transaction, [ locale ], templateName );
+
+      const mail_ = filler( template, texts, {
+        date,
+        name: `${name} ${surname}`,
+        userPhone: phone,
+        userEmail: email,
+        orderId,
+        buyable,
+        eventName: action_name,
+        eventLocations: locations,
+        domain: process.env.SELF_URL,
+        _
+      } );
+
+      mail.send(
+        email,
+        _( "success_payment" ),
+        "",
+        mail_
       );
     }
     else if( OrderStatus === 7 ) await transaction.query(
