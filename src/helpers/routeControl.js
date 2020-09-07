e3c2f99f8a6a467288c49e275d86d1f60c5a9618
FroZo <frozo02@yandex.ru>
2020-09-07 12:27:46 +0800

    [B] Fix

--- src/helpers/routeControl.js
+++ src/helpers/routeControl.js
@@ -161,7 +161,7 @@ export default ( server ) => {
   server.get( "/api/withdraws/:id/accept", secureAPI( "admin" ) );
   server.post( "/api/withdraws/:id/reject", secureAPI( "admin" ) );
 
-  // server.post( "/api/filterCrosses", secureAPI( "admin" ) );
+  server.post( "/api/filterCrosses", secureAPI( "admin" ) );
   server.put( "/api/filterCrosses/:id", secureAPI( "admin" ) );
   server.delete( "/api/filterCrosses/:id", secureAPI( "admin" ) );
 
